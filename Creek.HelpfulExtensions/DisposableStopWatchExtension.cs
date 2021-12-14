3940aaf966fecae854edb939124bbe3baa2a0515
Josh Creek <jcreek93@gmail.com>
2021-12-14 18:49:03 +0000

    feat(*): Add DisposableStopWatchExtension class

---
+++ Creek.HelpfulExtensions/DisposableStopWatchExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace Creek.HelpfulExtensions
+{
+    public class DisposableStopWatch : IDisposable
+    {
+        private ILogger _logger;
+        private Stopwatch _stopWatch;
+        private string _message;
+        private bool _useConsole;
+
+        public DisposableStopWatch(ILogger logger, string message = null, bool useConsole = false)
+        {
+            _logger = logger;
+            _stopWatch = new Stopwatch();
+            _message = message ?? string.Empty;
+            _useConsole = useConsole;
+
+            string startMessage = $"Start: {message}";
+
+            if (_useConsole)
+            {
+                _logger.LogInformation(startMessage);
+                Console.WriteLine(startMessage);
+            }
+            else
+            {
+                _logger.LogInformation(startMessage);
+            }
+
+            _stopWatch.Start();
+        }
+
+        public void Dispose()
+        {
+            _stopWatch.Stop();
+
+            string endMessage = $"Complete:{_message}: Elapsed: {_stopWatch.Elapsed}";
+
+            if (_useConsole)
+            {
+                _logger.LogInformation(endMessage);
+                Console.WriteLine(endMessage);
+            }
+            else
+            {
+                _logger.LogInformation(endMessage);
+            }
+        }
+    }
+
+    public static class DisposableStopWatchExtension
+    {
+        public static DisposableStopWatch DisposableStopWatch(this ILogger logger, string msg = null) => new DisposableStopWatch(logger, msg);
+    }
+}
